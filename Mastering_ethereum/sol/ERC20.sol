169c6269d3f3db5bb3de90c8c99f9164f02e86c6
shypang <smartdragon417@gmail.com>
2022-04-24 23:01:02 +0900

    erc20

---
+++ Mastering_ethereum/sol/ERC20.sol
@@ -0,0 +1,13 @@
+//ERC20 인터페이스 사양
+
+
+contract ERC20{
+    function totalSupply() constant returns (uint theTotalSupply);
+    function balanceOf(address _owner) constant returns (uint balance);
+    function transfer(address _to , uint _value) returns (bool suceess);
+    function transferFrom(address _from, address _to, uint _value) returns (bool success);
+    function approve(address _spender, uint _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint remaining);
+    event Transfer(address indexed _from , address indexed _to, uint _value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
